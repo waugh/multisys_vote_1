aabe23543060b2db12cbf51845a833346dd010e0
Jack Waugh <git.yipzdu@spamgourmet.com>
2012-12-17 07:09:41 -0500

    Change example.

--- raw_data.rb
+++ raw_data.rb
@@ -7,30 +7,30 @@ $example.data do |d|
     end
     v.candidate "Candidate B" do |c|
       c.rank  2
-      c.score 9
+      c.score 9.9
     end
     v.candidate "Candidate C" do |c|
       # c.rank  no rank
       c.score 0
     end
   end
-  d.voter "second group of 26 people" do |v|
-    v.weight 26
+  d.voter "second group" do |v|
+    v.weight 25
     v.candidate "Candidate B" do |c|
       c.rank  1
       c.score 10
     end
     v.candidate "Candidate A" do |c|
       c.rank  2
-      c.score 9
+      c.score 9.9
     end
     v.candidate "Candidate C" do |c|
       # c.rank  no rank
       c.score 0
     end
   end
-  d.voter "48 people" do |v|
-    v.weight 48
+  d.voter "Opposition group" do |v|
+    v.weight 49
     v.candidate "Candidate C" do |c|
       c.rank  1
       c.score 10
